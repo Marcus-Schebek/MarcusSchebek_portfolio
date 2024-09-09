2d886d706a82d1cdcf4efa0d6481cd22bf2ebe75
Marcus-Schebek <marcusschebek@gmail.com>
2024-09-08 22:30:28 -0300

    fix: mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
         hoverImageAlt="Imagem em Preto e Branco"
         title="nosconectados"
         description="NOSCONECTADOS é uma plataforma web desenvolvida para o gerenciamento de sensores IoT, com foco na otimização do manejo integrado de pragas na agricultura. Através da coleta e análise de dados em tempo real, o sistema auxilia os agricultores a tomarem decisões mais assertivas. Iniciado como uma iniciativa acadêmica da Universidade Federal de Pelotas (UFPel), o projeto conta com o apoio do Conselho Nacional de Desenvolvimento Científico e Tecnológico (CNPq), e busca impulsionar o desenvolvimento tecnológico em Pelotas e em toda a região sul do Rio Grande do Sul."
-        imagePosition="right" 
+        imagePosition="left" 
         slideEffect="fade-right"
       />
       <Project
@@ -40,7 +40,7 @@ function App() {
         hoverImageAlt="Imagem em Preto e Branco"
         title="Solida App"
         description="O Solida APP foi criado como um MVP no Hackathon HSClima, com o objetivo de conectar pessoas afetadas por crises climáticas a voluntários e autoridades. A plataforma utiliza tecnologias modernas como React para a interface, Node.js, Express e AdonisJS para o backend, além de Postgres para o armazenamento de dados. Oferece funcionalidades essenciais, como a identificação de áreas vulneráveis, facilitando a conexão entre necessitados e voluntários, além de integrar forças de segurança para uma resposta rápida e coordenada. O Solida APP busca promover solidariedade em momentos críticos, evidenciando a aplicação de soluções tecnológicas para superar desafios emergenciais."
-        imagePosition="left" 
+        imagePosition="right" 
         slideEffect="fade-left"
       />
             <Project
@@ -50,7 +50,7 @@ function App() {
         hoverImageAlt="Imagem em Preto e Branco"
         title="Eu, Yasmin"
         description="Este portfólio foi criado para uma aluna de jornalismo, utilizando VueJS. O objetivo foi desenvolver um site rápido e leve, sem custos, para promover e divulgar o trabalho de Yasmin (também conhecida como o 'amor da minha vida) "
-        imagePosition="right"
+        imagePosition="left"
         slideEffect="fade-right"
       />
       <ContactForm/>
